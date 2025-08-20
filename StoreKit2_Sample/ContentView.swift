53dcec7aa90ac1fcba9b1d65d3d64ca2af351784
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2025-08-20 08:35:31 -0700

    Initial Commit

---
+++ StoreKit2_Sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StoreKit2_Sample
+//
+//  Created by Tatsuya Moriguchi on 8/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StoreKit2_Sample/StoreKit2_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKit2_SampleApp.swift
+//  StoreKit2_Sample
+//
+//  Created by Tatsuya Moriguchi on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct StoreKit2_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
